180f8282215054c8d4fe3c0a4cbebb54f6e218e4
Alex Au <alex.au@health.nsw.gov.au>
2023-01-04 15:42:31 +1100

    got webmail to work

--- job_search.py
+++ job_search.py
@@ -6,27 +6,34 @@ import smtplib
 EMAIL_ADDRESS = os.environ.get('Gmail_Python_WebScrapping')
 EMAIL_PW = os.environ.get('Gmail_Python_PW')
 
+URL = "https://www.seek.com.au/python-jobs/in-Sydney-Olympic-Park-NSW-2127"
+
+python_jobs = []
+
 def find_python_jobs():
-    python_jobs_text = requests.get("https://www.seek.com.au/python-jobs/in-Sydney-Olympic-Park-NSW-2127").text
+    python_jobs_text = requests.get(URL).text
     soup = BeautifulSoup(python_jobs_text, 'lxml')
     jobs = soup.find_all('article', class_ = 'yvsb870 yvsb871 h3f08h1 _14uh9946i _14uh9947i _14uh9949m _14uh9948m _14uh9945a h3f08h5')
     for job in jobs:
         job_title = job.find('h3', class_ = 'yvsb870 _14uh9944u _1cshjhy0 _1cshjhyl _1d0g9qk4 _1cshjhys _1cshjhy21').text
-        company = job.find('span', class_ = 'yvsb870 _14uh9944u _1cshjhy0 _1cshjhy2 _1cshjhy21 _1d0g9qk4 _1cshjhyd').text[3:]      
+        python_jobs.append(f'Job Title:\t    {job_title}\n')
+        company = job.find('span', class_ = 'yvsb870 _14uh9944u _1cshjhy0 _1cshjhy2 _1cshjhy21 _1d0g9qk4 _1cshjhyd').text[3:]
+        python_jobs.append(f'Company:\t{company}\n')
         location = job.find('a', tabindex = '-1').text
+        python_jobs.append(f'Location:\t  {location}\n')
         salary = job.find('div', class_ = 'yvsb870 v28kuf0 v28kuf4 v28kuf2')
         if salary is not None:
             salary = salary.text
+        python_jobs.append(f'Salary:\t\t   {salary}\n')
         description1 = job.find('ul', class_ = 'yvsb870 yvsb873 v8nw070 v8nw074').text
         description2 = job.find('span', class_ = 'yvsb870 _14uh9944u _1cshjhy0 _1cshjhy1 _1cshjhy22 _1d0g9qk4 _1cshjhy7').text
-        print(f'''
-        Job Title:      {job_title}
-        Company:        {company}
-        Location:       {location}
-        Salary:         {salary}
-        Description:    {description1}
-                        {description2}''')
+        python_jobs.append(f'Description:\t{description1}\n\t\t\t{description2}\n')
+        node = job.h3.a['href']
+        weblink = f'https://www.seek.com.au{node}'
+        python_jobs.append(f'Weblink:\t{weblink}\n\n')
 
+find_python_jobs()
+python_jobs = ''.join(python_jobs)
 
 with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
     smtp.ehlo()
@@ -36,8 +43,7 @@ with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PW)
 
     subject = 'Python Developer Jobs'
-    body = find_python_jobs()
+    body = python_jobs
 
     msg = f'Subject: {subject}\n\n{body}'
-
     smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
--- tech_scrap.py
+++ tech_scrap.py
@@ -2,9 +2,16 @@ from bs4 import BeautifulSoup
 import requests
 from itertools import islice
 import time
+import os
+import smtplib
 
-def find_mx_keys():
-    ozbargain_mx_keys_text = requests.get('https://www.ozbargain.com.au/product/logitech-mx-keys').text
+EMAIL_ADDRESS = os.environ.get('Gmail_Python_WebScrapping')
+EMAIL_PW = os.environ.get('Gmail_Python_PW')
+
+URL = 'https://www.ozbargain.com.au/product/logitech-mx-keys'
+
+def find_product():
+    ozbargain_mx_keys_text = requests.get(URL).text
     soup = BeautifulSoup(ozbargain_mx_keys_text, 'lxml')
     banners = soup.find_all('div', class_ = 'n-right')
     limit = 5
@@ -23,7 +30,21 @@ def find_mx_keys():
 
 if __name__ == '__main__':
     while True:
-        find_mx_keys()
+        find_product()
         time_wait = 10
         print(f'Waiting one hour...')
         time.sleep(time_wait * 600)
+
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+
+    smtp.login(EMAIL_ADDRESS, EMAIL_PW)
+
+    subject = 'Python Developer Jobs'
+    body = find_product()
+
+    msg = f'Subject: {subject}\n\n{body}'
+
+    smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
